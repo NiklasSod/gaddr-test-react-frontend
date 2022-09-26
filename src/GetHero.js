83fb45e6c64e8f2846f0aa49ac1c0a1110dc9cfe
NiklasSod <nma.prob@gmail.com>
2022-09-26 14:53:41 +0200

    delete route

--- src/GetHero.js
+++ src/GetHero.js
@@ -4,6 +4,13 @@ import { useEffect, useState } from 'react';
 export default function GetHero() {
   const [heroes, setHeroes] = useState([]);
 
+  const deleteHero = (e, id) => {
+    e.preventDefault();
+    console.log(id)
+    axios.delete(`https://localhost:7227/api/superhero?id=${id}`);
+    window.location.reload();
+  };
+
   useEffect(() => {
     axios.get('https://localhost:7227/api/superhero').then((res) => {
       setHeroes(res.data);
@@ -17,9 +24,10 @@ export default function GetHero() {
         heroes.map(hero => {
           return (
             <div key={hero.id} style={{border: "1px solid black", width: "max-content", padding: "4px", margin: "4px"}}>
-              <p>{hero.heroName}</p>
+              <p>{"#" + hero.id + ": " + hero.heroName}</p>
               <p>{"Name: " + hero.firstName + " " + hero.lastName}</p>
               <p>{"From: " + hero.place}</p>
+              <button onClick={e => deleteHero(e, hero.id)}>Delete this hero</button>
             </div>
             
           )
